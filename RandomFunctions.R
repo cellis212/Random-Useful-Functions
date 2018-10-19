53fc291519d865e63ba3b4c198481f8de28249ae
Cameron Ellis <tuh48091@fox.temple.edu>
2018-10-19 13:27:30 -0400

    Loaded in random functions

---
+++ RandomFunctions.R
@@ -0,0 +1,93 @@
+require(plm)
+require(lmtest)
+require(lfe)
+require(stargazer)
+require(multiwayvcov)
+require(sandwich)
+require(DataExplorer)
+
+# Felm corrected when fes are nested in clusters
+felmNest <- function(form, data, ...)
+{
+  m <- felm(form, data, exactDOF = TRUE, keepCX = TRUE)
+  
+  mlm <- lm(m$cY ~ m$cX)
+  
+  clse <- cluster.vcov(mlm, m$clustervar, df_correction = FALSE) 
+  
+  ses <- coeftest(mlm , clse)
+  m$cse <- ses[2:nrow(ses),2]
+  m$ctval <- ses[2:nrow(ses),3]
+  m$cpval <- ses[2:nrow(ses),4]
+  return(m)
+}
+
+# The following are for rounding for table creation
+meanPr <- function(data, digits = 3)
+{
+  m <- mean(data, na.rm = TRUE)
+  return(formatC(m, digits = digits, format = "f"))
+}
+
+sdPr <- function(data, digits = 3)
+{
+  m <- sd(data, na.rm = TRUE)
+  return(formatC(m, digits = digits, format = "f"))
+}
+
+tdif <- function(var1, var2, digits = 2)
+{
+  m <- t.test(var1, var2)
+  return(formatC(m$statistic, digits = digits, format = "f"))
+}
+
+difPr <- function(var1, var2, digits = 2)
+{
+  m <- mean(var1, na.rm = TRUE) - mean(var2, na.rm = TRUE)
+  return(formatC(m, digits = digits, format = "f"))
+}
+
+# Get the mode of a data set
+getmode <- function(v) {
+  uniqv <- unique(v)
+  uniqv[which.max(tabulate(match(v, uniqv)))]
+}
+
+# Better config for DataExplorer
+config <- list(
+  "introduce" = list(),
+  "plot_str" = list(
+    "type" = "diagonal",
+    "fontSize" = 35,
+    "width" = 1000,
+    "margin" = list("left" = 350, "right" = 250)
+  ),
+  "plot_missing" = list(),
+  "plot_histogram" = list(),
+  "plot_bar" = list(),
+  "plot_correlation" = list("use" = "pairwise.complete.obs"),
+  "plot_boxplot" = list(),
+  "plot_scatterplot" = list()
+)
+
+#Tic/toc functionality
+tic <- function(gcFirst = TRUE, type=c("elapsed", "user.self", "sys.self"))
+{
+  type <- match.arg(type)
+  assign(".type", type, envir=baseenv())
+  if(gcFirst) gc(FALSE)
+  tic = 0
+  tic <- proc.time()[type]
+  assign(".tic", tic, envir=baseenv())
+  invisible(tic)
+}
+
+toc <- function()
+{
+  type <- get(".type", envir=baseenv())
+  toc = 0
+  toc <- proc.time()[type]
+  tic <- get(".tic", envir=baseenv())
+  print(toc - tic)
+  invisible(toc)
+}
