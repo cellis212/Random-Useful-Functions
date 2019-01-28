5fcfd016b7058c0d0281fae2865f208e16f59bce
Cameron M. Ellis <Cellis212@gmail.com>
2019-01-28 13:20:30 -0500

    Added to fix the labeled dataset problem

--- RandomFunctions.R
+++ RandomFunctions.R
@@ -135,7 +135,20 @@ fixStargazer <- function(tab, nmodels, scalesize = .8)
 }
 
   
-
+#### Fix the weird unlabeled dataset problem ####
+labelDataset <- function(data) {
+  correctLabel <- function(x) {
+    
+    if(!is.null(attributes(x)$labels)) {
+      class(attributes(x)$labels) <- typeof(x)
+    }
+    return(x)
+  }
+  for(i in colnames(data)) {
+    data[, i] <- correctLabel(data[, i])
+  }
+  return(data)
+}
 
 
 
